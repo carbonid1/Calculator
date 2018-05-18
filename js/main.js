024d0ee1137fa73b687e8553cf6a2e85f7a5bdb0
Сoriander <36999873+carbonid1@users.noreply.github.com>
2018-05-18 18:33:53 +0300

    "1.1"

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ function Calculator() {
 
 Calculator.prototype.token = function(token) {
   if (this.prevTokenIsAction && token == "/" ||
-      this.prevTokenIsAction && token == "*" ||
-      this.prevTokenIsAction && token == "+" ||
-      this.prevTokenIsAction && token == "-" ) {
+    this.prevTokenIsAction && token == "*" ||
+    this.prevTokenIsAction && token == "+" ||
+    this.prevTokenIsAction && token == "-") {
+    // do nothing
   } else if (token == "/" || token == "*" || token == "+" || token == "-") {
     this.currentNumber = `${token}`;
     this.expression += `${token}`;
@@ -21,7 +22,7 @@ Calculator.prototype.token = function(token) {
     this.finalNum = this.evaluate();
     this.currentNumber = this.finalNum;
     this.expression += "=" + this.finalNum;
-    if(this.currentNumber.length > 8 || this.expression.length > 20) {
+    if (this.currentNumber.length > 8 || this.expression.length > 20) {
       upperScreenP.textContent = "0";
       lowerScreenP.textContent = "digit limit met";
       this.currentNumber = "";
@@ -41,7 +42,10 @@ Calculator.prototype.token = function(token) {
     upperScreenP.textContent = "0";
     lowerScreenP.textContent = "0";
   } else if (token == "CE") {
-    if(this.prevTokenIsEquals) {
+    if (this.expression == "") {
+      upperScreenP.textContent = "0";
+      lowerScreenP.textContent = "0";
+    } else if (this.prevTokenIsEquals) {
       this.currentNumber = "";
       this.expression = "";
       upperScreenP.textContent = "0";
@@ -52,12 +56,19 @@ Calculator.prototype.token = function(token) {
       upperScreenP.textContent = this.currentNumber;
       lowerScreenP.textContent = this.expression;
       this.prevTokenIsAction = false;
-    } else if (this.expression != "0") {
-      let lastNumbers = this.findLastNumber();
-      this.expression = this.expression.slice(0, -(lastNumbers.length)-1);
-      this.currentNumber = this.evaluate();
-      upperScreenP.textContent = this.currentNumber;
-      lowerScreenP.textContent = this.expression;
+    } else {
+      if (this.findNumbersAndDots() == this.expression.length) {
+        this.currentNumber = "";
+        this.expression = "";
+        upperScreenP.textContent = "0";
+        lowerScreenP.textContent = "0";
+      } else {
+        let lastNumbers = this.findLastNumber();
+        this.expression = this.expression.slice(0, -(lastNumbers.length) - 1);
+        this.currentNumber = this.evaluate();
+        upperScreenP.textContent = this.currentNumber;
+        lowerScreenP.textContent = this.expression;
+      }
     }
   } else if (token == "." && this.findDotsAmount() >= 1) {
     //do nothing
@@ -90,10 +101,16 @@ Calculator.prototype.findLastNumber = function() {
   return arr[0];
 }
 
+Calculator.prototype.findNumbersAndDots = function() {
+  let str = this.expression;
+  let arr = str.match(/[.0-9]/g);
+  return arr.length;
+}
+
 Calculator.prototype.findDotsAmount = function() {
   let str = this.currentNumber;
   let arr = str.match(/\./g);
-  if(arr == null) {
+  if (arr == null) {
     return 0;
   } else {
     return arr.length;
@@ -133,68 +150,68 @@ document.addEventListener('keydown', function(event) {
       break;
     case "NumpadDivide":
     case "Slash":
-        calculator.token("/");
-        break;
+      calculator.token("/");
+      break;
     case "NumpadSubtract":
     case "Minus":
-        calculator.token("-");
-        break;
+      calculator.token("-");
+      break;
     case "NumpadAdd":
-        calculator.token("+");
-        break;
+      calculator.token("+");
+      break;
     case "Period":
-        calculator.token(".");
-        break;
+      calculator.token(".");
+      break;
     case "Equal":
     case "Enter":
     case "NumpadEnter":
-        calculator.token("=");
-        break;
+      calculator.token("=");
+      break;
     case "Escape":
-        calculator.token("AC");
-        break;
+      calculator.token("AC");
+      break;
     case "Backspace":
-        calculator.token("CE");
-        break;
-      case "Digit0":
-      case "Numpad0":
-        calculator.token("0");
-        break;
+      calculator.token("CE");
+      break;
+    case "Digit0":
+    case "Numpad0":
+      calculator.token("0");
+      break;
     case "Digit1":
     case "Numpad1":
-        calculator.token("1");
-        break;
+      calculator.token("1");
+      break;
     case "Digit2":
     case "Numpad2":
-        calculator.token("2");
-        break;
+      calculator.token("2");
+      break;
     case "Digit3":
     case "Numpad3":
-        calculator.token("3");
-        break;
+      calculator.token("3");
+      break;
     case "Digit4":
     case "Numpad4":
-        calculator.token("4");
-        break;
+      calculator.token("4");
+      break;
     case "Digit5":
     case "Numpad5":
-        calculator.token("5");
-        break;
+      calculator.token("5");
+      break;
     case "Digit6":
     case "Numpad6":
-        calculator.token("6");
-        break;
+      calculator.token("6");
+      break;
     case "Digit7":
     case "Numpad7":
-        calculator.token("7");
-        break;
+      calculator.token("7");
+      break;
     case "Digit8":
     case "Numpad8":
-        calculator.token("8");
-        break;
+      calculator.token("8");
+      break;
     case "Digit9":
     case "Numpad9":
-        calculator.token("9");
-        break;
+      calculator.token("9");
+      break;
   }
 });
